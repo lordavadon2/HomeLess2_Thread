4c7b987f22bb2291db14b4c846c40641bb4ea038
lordavadon2 <lordavadon22@gmail.com>
2018-04-10 12:22:42 +0300

    1

--- src/com/brainacad/hw/Main.java
+++ src/com/brainacad/hw/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
 
     public static void main(String[] args) {
-        String path = "D:\\250GB"; //Введите ваш путь
+        String path = "qwerty"; //Введите ваш путь
         try {
             new DirView(path).print();
         } catch (IOException e) {
